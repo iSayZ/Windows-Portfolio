141001097f81ff41cdc57f9a92ac1a7b473f1993
iSayZ <estrine.alexis@gmail.com>
2024-12-24 15:03:28 +0100

    fix: DoubleClick to open an app

--- src/app/components/AppIcon/component.tsx
+++ src/app/components/AppIcon/component.tsx
@@ -5,7 +5,7 @@ const AppIcon: React.FC<AppIconProps> = ({ icon, name, onClick }) => {
   return (
     <div
       className="p-2 rounded-sm flex flex-col items-center cursor-pointer hover:bg-accent"
-      onClick={onClick}
+      onDoubleClick={onClick}
     >
       <div className="h-8 w-16 rounded-full flex items-center justify-center text-foreground">
         {/* <img src={icon} alt={name} className="h-10 w-10" /> */}
